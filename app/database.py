745887a188fe880470e961ae2e2974404328626a
jagrutiii05 <202151067@iiitvadodara.ac.in>
2025-01-27 17:17:15 +0530

    database path made relative

--- app/database.py
+++ app/database.py
@@ -3,9 +3,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = "sqlite:///./data/new_test.db"
+# DATABASE_URL = "sqlite:///./data/new_test.db"
 
-os.makedirs(os.path.dirname(DATABASE_URL.split("///")[1]), exist_ok=True)
+# os.makedirs(os.path.dirname(DATABASE_URL.split("///")[1]), exist_ok=True)
+
+# Get the absolute path of the current script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the SQLite database path relative to the project directory
+DATABASE_PATH = os.path.join(BASE_DIR, "../data/new_test.db")
+
+# Define the database URL
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
